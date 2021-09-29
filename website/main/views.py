f6dc3e284292170914c31c69ec202c320208b458
hayeonkim <hayeon.data@gmail.com>
2021-09-29 20:59:46 +0900

    Update website

--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .resultpy import model_load
+from django.core.files.storage import FileSystemStorage
+
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def index(request):
+    pred_y = model_load()
+    return render(request, 'main/index.html', {'pred_y': pred_y})
