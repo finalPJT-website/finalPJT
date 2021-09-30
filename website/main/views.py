ee29fb150d3f18f610297485205fbcfb29109ee5
hayeonkim <hayeon.data@gmail.com>
2021-09-30 16:04:02 +0900

    Update website

--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .resultpy import model_load
+from .lstm import model_load2
 from django.core.files.storage import FileSystemStorage
 
 
@@ -11,3 +12,8 @@ def index(request):
 def index(request):
     pred_y = model_load()
     return render(request, 'main/index.html', {'pred_y': pred_y})
+
+
+def index(request):
+    graph = model_load2()
+    return render(request, 'main/index.html', {'graph': graph})
